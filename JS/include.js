ce6352392d0d1fe0a5cf642358cbb0381b48d47e
Ethan <ethan.armstrong111@gmail.com>
2026-07-27 22:11:14 +1200

    Version 2: Fully implimented base files with own css pages using scope tag

--- JS/include.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("[data-include]").forEach(async (el) => {
-    const file = el.getAttribute("data-include");
-    if (file) {
-      const res = await fetch(file);
-      if (res.ok) el.innerHTML = await res.text();
-    }
-  });
-});
